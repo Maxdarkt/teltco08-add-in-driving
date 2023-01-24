c1a67f066f00c5ae32343e092ffe56730dd2296b
Maxence Tourneux <tourneuxmaxence@gmail.com>
2023-01-24 16:26:17 +0100

    improve form - write data in progress : userInfos and calculate date expiration

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,31 @@
 <template>
-  <div id="app" class="bg-gray-200">
+  <div id="app" class="bg-gray-200 h-screen flex flex-col justify-start items-center">
     <!-- Header -->
-    <div class="p-12 bg-blue-400 flex justify-center items-center">
+    <div class="w-full p-4 bg-blue-400 flex justify-center items-center">
       <h1 class="text-xl text-gray-50">
         Autorisation de conduite
       </h1>
     </div>
     <!-- general informations --> 
-    <div class="bg-gray-50 p-4">
+    <div class="bg-gray-50 p-4 max-[200px]">
       <div>
         <h2 class="text-md font-normal">
           Informations genérales:
         </h2>
         <!-- Responsible input -->
-        <div>
-          <label for="responsible" class="text-sm font-thin">Nom et Prénom du Délégant :</label>
-          <input id="responsible" type="text" v-model="responsible" class="bg-transparent" required/>
+        <div class="flex flex-col mt-4">
+          <label for="responsible" class="block mb-2 text-sm font-extralight text-gray-900">Nom et Prénom du Délégant :</label>
+          <input id="responsible" type="text" v-model="responsible" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5" required/>
         </div>
         <!-- limit date input -->
-        <div>
-          <label for="limitDate" class="text-sm font-thin">Date d'expiration (en jrs) :</label>
-          <input id="limitDate" type="number" min="0" placeholder="0" v-model="limitDate" class="bg-transparent" required/>
+        <div class="flex flex-col mt-4">
+          <label for="limitDate" class="block mb-2 text-sm font-extralight">Date d'expiration (en jrs) :</label>
+          <input id="limitDate" type="number" min="0" placeholder="0" v-model="limitDate" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5" required/>
+        </div>
+        <!-- date authorization input -->
+        <div class="flex flex-col mt-4">
+          <label for="dateAuthorization" class="block mb-2 text-sm font-extralight">Date du document :</label>
+          <input id="dateAuthorization" type="date" v-model="dateAuthorization" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5" required/>
         </div>
       </div>
       <!-- select user row input -->
@@ -28,21 +33,32 @@
         <h2 class="text-md font-normal">
           Conducteur/trice d'engins :
         </h2>
-        <label for="row" class="text-sm font-thin">Entrer le numéro de ligne de la personne :</label>
-        <input id="row" type="number" min="3" placeholder="3" v-model="rowInput" class="bg-transparent" @change="readData()" />
+        <div class="flex flex-col mt-4">
+          <label for="row" class="block mb-2 text-sm font-extralight">Entrer le numéro de ligne de la personne :</label>
+          <input id="row" type="number" min="3" placeholder="3" v-model="rowInput" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-1.5" @change="readData()" />
+        </div>
       </div>
       <!-- user selected and button for edit document -->
       <div v-if="userDataChecked" class="rounded-md border border-blue-500 flex flex-col items-center mt-4 p-4">
-        <p>Utilisateur sélectionné :</p>
-        <p v-if="userDataInfos.isPresent">
-          {{ userDataInfos.lastName }} {{ userDataInfos.firstName }}
-        </p>
-        <button v-if="userDataInfos.isPresent" class="bg-blue-600 text-gray-200 py-2 px-3 rounded-md transition duration-500 hover:bg-blue-800 hover:text-gray-50 mt-4" @click="editDocument">
-          Générer le document
-        </button>
-        <p v-else>
-          {{ userDataInfos.lastName }} {{ userDataInfos.firstName }} ne fait plus parti des effectifs.
-        </p>
+        <h3 class="w-full text-center text-md">Utilisateur sélectionné :</h3>
+        <!-- display the user selected and button "edit document" -->
+        <div v-if="userDataInfos.isPresent && medicalExaminationChecked" class="w-full flex flex-col items-center">
+          <p class="w-full text-center text-blue-600 mt-2">
+            {{ userDataInfos.lastName }} {{ userDataInfos.firstName }}
+          </p>
+          <button v-if="userDataInfos.isPresent" class="bg-blue-600 text-gray-200 py-2 px-3 rounded-md transition duration-500 hover:bg-blue-800 hover:text-gray-50 mt-4" @click="editDocument">
+            Générer le document
+          </button>
+        </div>
+        <!-- error message -->
+        <div v-else>
+          <p v-if="!userDataInfos.isPresent" class="text-sm text-red-500">
+            {{ userDataInfos.lastName }} {{ userDataInfos.firstName }} ne fait plus parti des effectifs.
+          </p>
+          <p v-else-if="!medicalExaminationChecked" class="text-sm text-red-500">
+            {{ userDataInfos.lastName }} {{ userDataInfos.firstName }} n'a pas de date de visite médicale valide.
+          </p>
+        </div>
       </div>
       <div v-else-if="rowInput > 3" class="rounded-md border border-red-500 mt-4 p-4">
         <p class="text-red-500 font-medium">
@@ -60,9 +76,10 @@ export default {
   name: 'App',
   data() {
     return {
-      responsible: null,
+      responsible: "Alexander Heim",
       limitDate: 7,
       rowInput: 3,
+      dateAuthorization: Date.now(),
       keyDataInfos : ["isPresent", "team", "lastName", "firstName", "position", "company" ],
       keyDataDates: [ "workAtHeight", "AIPR", "shotCrete", "scaffoldingReception", "R482CatA", "R482CatB1", "R482CatB2", "R482CatB3"],
       medicalExamination: null,
@@ -133,7 +150,6 @@ export default {
 
         this.userDataInfos.isPresent = this.userDataInfos.isPresent === "Présent";
 
-        console.log(this.userDataInfos);
       } else if (type === "dates") {
         this.userDataDates = {};
 
@@ -141,7 +157,6 @@ export default {
           this.userDataDates[this.keyDataDates[i]] = rangeValues[0][i];
         }
 
-        console.log(this.userDataDates);
         this.transformDateExcel("dates");
       } else {
         console.log('error type in initObject');
@@ -158,7 +173,6 @@ export default {
             this.userDataDates[item] = moment.fromOADate(this.userDataDates[item]);
           }
         })
-        console.log('transformDateExcel dates', this.userDataDates);
         this.isTheDateValid(type);
 
       } else if(type === "medical") {
@@ -167,8 +181,8 @@ export default {
           this.isTheDateValid(type);
         } else {
           this.medicalExamination = null;
+          this.medicalExaminationChecked = false;
         }
-        console.log('transformDateExcel medical', this.medicalExamination);
       }
     },
     // we check if the examination dates are valid
@@ -187,10 +201,8 @@ export default {
             this.userDataDatesChecked[item] = false;
           }
         })
-        console.log(this.userDataDatesChecked);
       } else if(type === "medical") {
         this.medicalExaminationChecked = this.diffDate(this.medicalExamination);
-        console.log('medicalChecked', this.medicalExaminationChecked);
       }
     },
     // we calculate the difference between a date and today
@@ -205,13 +217,53 @@ export default {
       }
     },
     editDocument() {
-      this.writeUserDataInfos();
+      const minExamDate = this.calculateMinDate();
+      this.writeUserDataInfos(minExamDate);
+      console.log(this.userDataDatesChecked);
+      console.log(this.userDataDates);
     },
-    writeUserDataInfos() {
+    calculateMinDate() {
+      const arrayValidDate = [];
+      Object.keys(this.userDataDates).forEach(item => {
+  
+        if(typeof this.userDataDates[item] === 'object') {
+          if(this.diffDate(this.userDataDates[item])) {
+            arrayValidDate.push(moment(this.userDataDates[item]))
+          }
+        }
+
+      })
+      return moment.min(arrayValidDate);
+    },
+    writeUserDataInfos(minExamDate) {
       window.Excel.run((context) => {
+        // write lastName
         const ws = context.workbook.worksheets.getItem('TEMPLATE');
-        const range = ws.getRange("I8");
-        range.values = [[44]];
+        const rangeLastName = ws.getRange("D5");
+        rangeLastName.values = [[this.userDataInfos.lastName]];
+        // write firstName
+        const rangeFirstName = ws.getRange("L5");
+        rangeFirstName.values = [[this.userDataInfos.firstName]];
+        // write date
+        const dateAuthorizationFormat = moment(this.dateAuthorization).toOADate();
+        // S5
+        const rangeDateAuthorization = ws.getRange("S5");
+        rangeDateAuthorization.values = [[dateAuthorizationFormat]];
+        rangeDateAuthorization.numberFormat = [["dd/[$-409]mm/yyyy"]];
+        // E58
+        const rangeDateDelivered = ws.getRange("E58");
+        rangeDateDelivered.values = [[dateAuthorizationFormat]];
+        rangeDateDelivered.numberFormat = [["dd/[$-409]mm/yyyy"]];
+        // write medical examination
+        const dateExaminationMedicalFormat = moment(this.medicalExamination).toOADate();
+        const rangeDateExaminationMedical = ws.getRange("I8");
+        rangeDateExaminationMedical.values = [[dateExaminationMedicalFormat]];
+        rangeDateExaminationMedical.numberFormat = [["dd/[$-409]mm/yyyy"]];
+        // write expiration date
+        const dateExpirationFormat = moment(minExamDate).toOADate();
+        const rangeDateExpiration = ws.getRange("S8");
+        rangeDateExpiration.values = [[dateExpirationFormat]];
+        rangeDateExpiration.numberFormat = [["dd/[$-409]mm/yyyy"]];
         return context.sync();
       });
     }
